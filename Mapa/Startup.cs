6a89a46d9af1c8c1531c4dcd2b21972389579f2a
dionis <d.spicyn@gmail.com>
2020-10-25 23:06:34 +0300

    fix

--- Mapa/Startup.cs
+++ Mapa/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.HttpOverrides
+using Microsoft.AspNetCore.HttpOverrides;
 using Mapa.Services;
 
 namespace Mapa

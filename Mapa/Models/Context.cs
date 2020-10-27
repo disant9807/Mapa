61d5a98f49160e694e0b9904321acd8ab8001391
dionis <d.spicyn@gmail.com>
2020-10-27 15:55:25 +0300

    Add db context and models User

---
+++ Mapa/Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Mapa.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ Mapa/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mapa.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+    }
+}
